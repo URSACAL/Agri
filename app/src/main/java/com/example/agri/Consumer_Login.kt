8bc41269b16f76fa448c59e78669cc0bddf19ad6
Jonard Ursal <jonardursal@gmail.com>
2023-12-04 04:08:28 -0800

    first commit

---
+++ app/src/main/java/com/example/agri/Consumer_Login.kt
@@ -0,0 +1,11 @@
+package com.example.agri
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Consumer_Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consumer_login)
+    }
+}
