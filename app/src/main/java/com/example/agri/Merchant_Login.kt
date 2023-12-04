8c072ce53c1458b5f14af2b8fd8b26d5026421d8
Jonard Ursal <jonardursal@gmail.com>
2023-12-04 04:50:26 -0800

    first commit

--- app/src/main/java/com/example/agri/Merchant_Login.kt
+++ app/src/main/java/com/example/agri/Merchant_Login.kt
@@ -1,80 +1,96 @@
 package com.example.agri
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Merchant_Login : AppCompatActivity() {
-//
-//    val loginuser = findViewById<EditText>(R.id.log_user)
-//    val loginpass = findViewById<EditText>(R.id.log_pass)
-//    val loginbutton = findViewById<Button>(R.id.login)
+
+    private lateinit var loginuser: EditText
+    private lateinit var loginpass: EditText
+    private lateinit var loginbutton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_merchant_login)
 
-//        loginbutton.setOnClickListener(View.OnClickListener {
-//            if (!validateUsername() or !validatePassword()) {
-//            } else {
-//                checkUser()
-//            }
-//        })
+        // Initialize UI elements
+        loginuser = findViewById(R.id.log_user)
+        loginpass = findViewById(R.id.log_pass)
+        loginbutton = findViewById(R.id.login)
+
+        loginbutton.setOnClickListener(View.OnClickListener {
+            if (!validateUsername() or !validatePassword()) {
+                // Handle validation failure if needed
+            } else {
+                checkUser()
+            }
+        })
     }
 
-//    fun validateUsername(): Boolean {
-//        val `val` = loginuser!!.text.toString()
-//        return if (`val`.isEmpty()) {
-//            loginuser.error = "Username cannot be empty"
-//            false
-//        } else {
-//            loginuser.error = null
-//            true
-//        }
-//    }
-//
-//    fun validatePassword(): Boolean {
-//        val `val` = loginpass!!.text.toString()
-//        return if (`val`.isEmpty()) {
-//            loginpass.error = "Password cannot be empty"
-//            false
-//        } else {
-//            loginpass.error = null
-//            true
-//        }
-//    }
-//
-//    fun checkUser() {
-//        val userUsername = loginuser!!.text.toString().trim { it <= ' ' }
-//        val userPassword = loginpass!!.text.toString().trim { it <= ' ' }
-//        val reference = FirebaseDatabase.getInstance().getReference("Account")
-//        val checkUserDatabase = reference.orderByChild("email").equalTo(userUsername)
-//        checkUserDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                if (snapshot.exists()) {
-//                    loginuser.error = null
-//                    val passwordFromDB = snapshot.child(userUsername).child("password").getValue(
-//                        String::class.java
-//                    )
-//                    if (passwordFromDB == userPassword) {
-//                        loginuser.error = null
-//                        val emailFromDB = snapshot.child(userUsername).child("email").getValue(
-//                            String::class.java
-//                        )
-//                        val intent = Intent(this@Merchant_Login, MerchantTagline::class.java)
-//                        intent.putExtra("email", emailFromDB)
-//                        intent.putExtra("password", passwordFromDB)
-//                        startActivity(intent)
-//                    } else {
-//                        loginpass.error = "Invalid Credentials"
-//                        loginpass.requestFocus()
-//                    }
-//                } else {
-//                    loginuser.error = "User does not exist"
-//                    loginuser.requestFocus()
-//                }
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {}
-//        })
-//    }
+    fun validateUsername(): Boolean {
+        val value = loginuser.text.toString()
+        return if (value.isEmpty()) {
+            loginuser.error = "Username cannot be empty"
+            false
+        } else {
+            loginuser.error = null
+            true
+        }
+    }
+
+    fun validatePassword(): Boolean {
+        val value = loginpass.text.toString()
+        return if (value.isEmpty()) {
+            loginpass.error = "Password cannot be empty"
+            false
+        } else {
+            loginpass.error = null
+            true
+        }
+    }
+
+    fun checkUser() {
+        val userUsername = loginuser.text.toString().trim()
+        val userPassword = loginpass.text.toString().trim()
+        val reference = FirebaseDatabase.getInstance().getReference("Account")
+        val checkUserDatabase = reference.orderByChild("email").equalTo(userUsername)
+        checkUserDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    loginuser.error = null
+                    val passwordFromDB = snapshot.child(userUsername).child("password").getValue(
+                        String::class.java
+                    )
+                    if (passwordFromDB == userPassword) {
+                        loginuser.error = null
+                        val emailFromDB = snapshot.child(userUsername).child("email").getValue(
+                            String::class.java
+                        )
+                        val intent = Intent(this@Merchant_Login, MerchantTagline::class.java)
+                        intent.putExtra("email", emailFromDB)
+                        intent.putExtra("password", passwordFromDB)
+                        startActivity(intent)
+                    } else {
+                        loginpass.error = "Invalid Credentials"
+                        loginpass.requestFocus()
+                    }
+                } else {
+                    loginuser.error = "User does not exist"
+                    loginuser.requestFocus()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle onCancelled if needed
+            }
+        })
+    }
 }
